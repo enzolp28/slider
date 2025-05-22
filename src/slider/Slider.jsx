91531c794461add31c97d7244b7ee4c1e9e5aeee
votre_nom <laposta.enzo@gmail.com>
2025-05-22 13:46:47 +0200

    slider actif

--- src/slider/Slider.jsx
+++ src/slider/Slider.jsx
@@ -2,29 +2,33 @@ import { useState } from "react"
 import leftChevron from "../assets/left-arrow.svg"
 import rightChevron from "../assets/right-arrow.svg"
 import "./Slider.css"
+import sliderData from "../data/sliderData"
 
 export default function Slider() {
-    const [photo, setPhoto] = useState([
-        "img-1.jpg",
-        "img-2.jpg",
-        "img-3.jpg",
-        "img-4.jpg",
-        "img-5.jpg",
-    ])
 
-    function handleClick() {
-        setPhoto
+    const [sliderIndex, setSliderIndex] = useState(1)
+
+    function handleClick(indexPayload) {
+        let newState
+        if (indexPayload + sliderData > sliderData.length) {
+            newState = 1
+        } else if (indexPayload + sliderIndex < 1) {
+            newState = sliderData.length
+        } else {
+            newState = indexPayload + sliderIndex
+        }
+        setSliderIndex(newState)
     }
     return (
         <>
-            <p className="index-info">3/5</p>
+            <p className="index-info">{sliderIndex} / {sliderData.length}</p>
             <div className="slider">
-                <p className="image-info">Bedroom</p>
-                <img className="slider-image" src="/images/img-3.jpg" alt="Bedroom" />
-                <button className="nav-button prev-button" onClick={handleClick}>
+                <p className="image-info">{sliderData.find(obj => obj.id === sliderIndex).description}</p>
+                <img className="slider-image" src={`/images/img-${sliderIndex}.jpg`} alt="Bedroom" />
+                <button className="nav-button prev-button" onClick={() => handleClick(-1)}>
                     <img src={leftChevron} alt="previous image" />
                 </button>
-                <button className="nav-button next-button" onClick={handleClick}>
+                <button className="nav-button next-button" onClick={() => handleClick(1)}>
                     <img src={rightChevron} alt="next image" />
                 </button>
             </div>
