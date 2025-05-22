1722ce1c00bf698de95576fb4b767828931678d7
votre_nom <laposta.enzo@gmail.com>
2025-05-22 14:03:14 +0200

    slider with setInterval()

--- src/slider/Slider.jsx
+++ src/slider/Slider.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import leftChevron from "../assets/left-arrow.svg"
 import rightChevron from "../assets/right-arrow.svg"
 import "./Slider.css"
@@ -9,16 +9,35 @@ export default function Slider() {
     const [sliderIndex, setSliderIndex] = useState(1)
 
     function handleClick(indexPayload) {
-        let newState
-        if (indexPayload + sliderData > sliderData.length) {
-            newState = 1
-        } else if (indexPayload + sliderIndex < 1) {
-            newState = sliderData.length
-        } else {
-            newState = indexPayload + sliderIndex
-        }
-        setSliderIndex(newState)
+        // let newState
+        // if (indexPayload + sliderData > sliderData.length) {
+        //     newState = 1
+        // } else if (indexPayload + sliderIndex < 1) {
+        //     newState = sliderData.length
+        // } else {
+        //     newState = indexPayload + sliderIndex
+        // }
+        // setSliderIndex(newState)
+
+        setSliderIndex(state => {
+            if (indexPayload + state > sliderData.length) {
+                return 1
+            } else if (indexPayload + state < 1) {
+                return sliderData.length
+            } else {
+                return state + indexPayload
+            }
+        })
     }
+
+    useEffect(() => {
+        const intervalId = setInterval(() => handleClick(1), 2000)
+
+        return (() => clearInterval(intervalId))
+
+    }, [])
+
+
     return (
         <>
             <p className="index-info">{sliderIndex} / {sliderData.length}</p>
